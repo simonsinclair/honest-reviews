5f756d08d0f3e1c6c2b3026d9dfa09733b935587
Simon Sinclair <simon@simonsinclair.com>
2022-05-31 23:13:23 +0100

    Remove background container from Footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,7 +1,5 @@
 export const Footer = () => (
-  <div className="bg-gray-100">
-    <footer className="container mx-auto flex items-center justify-between gap-4 px-4 py-8">
-      <small>&copy; 2022 Honest Reviews.</small>
-    </footer>
-  </div>
+  <footer className="container mx-auto flex items-center justify-between gap-4 px-4 py-8">
+    <small>&copy; 2022 Honest Reviews.</small>
+  </footer>
 );
