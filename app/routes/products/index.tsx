ad33556a17633b72f8c1b6919e7d544489da6b3a
Simon Sinclair <simon@simonsinclair.com>
2022-06-01 00:43:36 +0100

    Improve Products page layout

--- app/routes/products/index.tsx
+++ app/routes/products/index.tsx
@@ -18,23 +18,37 @@ const ProductsPage = () => {
   const { products } = useLoaderData<LoaderData>();
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="mb-4">Products</h1>
-      {products.length > 0 ? (
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-          {products.map(({ id, name, description }) => (
-            <div className="space-y-2 rounded bg-gray-100 p-4" key={id}>
-              <h2>
-                <Link to={`/products/${id}`}>{name}</Link>
-              </h2>
-              <p>{description}</p>
-            </div>
-          ))}
+    <>
+      <div className="mb-4 bg-white shadow-sm">
+        <div className="container mx-auto p-4">
+          <h1>Products</h1>
         </div>
-      ) : (
-        <p>There are no products to display.</p>
-      )}
-    </div>
+      </div>
+      <div className="container mx-auto px-4">
+        {products.length > 0 ? (
+          <div className="grid grid-cols-12 gap-4">
+            {products.map(({ id, name, description }) => (
+              <div
+                className="col-span-full space-y-2 rounded-lg bg-white p-4 shadow-sm lg:col-span-8 lg:col-start-3"
+                key={id}
+              >
+                <h2>
+                  <Link
+                    to={`/products/${id}`}
+                    className="font-bold hover:underline"
+                  >
+                    {name}
+                  </Link>
+                </h2>
+                <p>{description}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>There are no products to display.</p>
+        )}
+      </div>
+    </>
   );
 };
 
