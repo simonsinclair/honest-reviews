526811918781424ea28af5cba7f6ba225b12f852
Simon Sinclair <simon@simonsinclair.com>
2022-06-01 23:27:48 +0100

    Add Honest Reviews 'logo'

--- app/components/Masthead/index.tsx
+++ app/components/Masthead/index.tsx
@@ -1,9 +1,15 @@
 import { Link } from '@remix-run/react';
 
+import { AwardIcon } from '~/components/Icons';
+
 export const Masthead = () => (
   <div className="bg-sky-900">
     <header className="container mx-auto flex items-center justify-between gap-4 px-4 antialiased">
-      <Link to="/products" className="text-lg text-white hover:text-sky-200">
+      <Link
+        to="/products"
+        className="flex items-center gap-1.5 text-lg text-white hover:text-sky-200"
+      >
+        <AwardIcon aria-hidden />
         <strong>Honest Reviews</strong>
       </Link>
       <nav>
