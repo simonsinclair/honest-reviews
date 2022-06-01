487fbde3f8a7ff9d453b0d4844d8bf7a99d67460
Simon Sinclair <simon@simonsinclair.com>
2022-06-01 23:27:10 +0100

    Add components/Icons

---
+++ app/components/Icons/Award.tsx
@@ -0,0 +1,21 @@
+import type { HTMLAttributes } from 'react';
+
+type Props = HTMLAttributes<SVGElement>;
+
+export const AwardIcon = ({ ...attributes }: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...attributes}
+  >
+    <circle cx="12" cy="8" r="7"></circle>
+    <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+  </svg>
+);
---
+++ app/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export * from './Award';
+export * from './ChevronRight';
+export * from './ChevronLeft';
+export * from './Edit';
+export * from './Star';
