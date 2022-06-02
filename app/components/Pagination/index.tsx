bae2bacf2f37ebd7719e81a258f1f3045e55c477
Simon Sinclair <simon@simonsinclair.com>
2022-06-02 17:23:47 +0100

    Add isCurrentPage behaviour to Pagination

--- app/components/Pagination/index.tsx
+++ app/components/Pagination/index.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from '@remix-run/react';
+import { Link } from '@remix-run/react';
 import pgr from '@simonsinclair/pgr';
 
 type Props = {
@@ -9,17 +9,25 @@ type Props = {
 
 export const Pagination = ({ totalPages, currentPage, length = 5 }: Props) => (
   <ol className="flex gap-1">
-    {pgr(totalPages, currentPage, length, (pageNumber) => (
-      <li key={pageNumber}>
-        <NavLink
-          to={`?page=${pageNumber}`}
-          className="block rounded-full p-2 font-medium leading-none shadow transition-colors hover:shadow-md"
-        >
-          <div className="flex h-6 w-6 items-center justify-center">
-            {pageNumber}
-          </div>
-        </NavLink>
-      </li>
-    ))}
+    {pgr(totalPages, currentPage, length, (pageNumber) => {
+      const isCurrentPage = currentPage === pageNumber;
+      return (
+        <li key={pageNumber}>
+          <Link
+            to={`?page=${pageNumber}`}
+            aria-current={isCurrentPage ? 'page' : undefined}
+            className={
+              isCurrentPage
+                ? 'block rounded-full p-2 font-medium leading-none shadow shadow-sky-400 hover:shadow-sky-400'
+                : 'block rounded-full p-2 font-medium leading-none shadow hover:shadow-sky-400'
+            }
+          >
+            <div className="flex h-6 w-6 items-center justify-center">
+              {pageNumber}
+            </div>
+          </Link>
+        </li>
+      );
+    })}
   </ol>
 );
