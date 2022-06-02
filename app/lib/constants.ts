66b1d8bb9f04ece81aa838b1e8dd65b10ea40a41
Simon Sinclair <simon@simonsinclair.com>
2022-06-02 17:28:05 +0100

    Document constant values

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,2 +1,9 @@
+/**
+ * The default number of items to take in a Prisma paginated query.
+ */
 export const DEFAULT_TAKE = 10;
+
+/**
+ * Our application's maximum-supported review rating.
+ */
 export const RATING_MAX = 5;
