0641042c1958dec0605f8e5325758c3d83e301c9
Simon Sinclair <simon@simonsinclair.com>
2022-06-03 13:21:39 +0100

    Render StarRatingInput inputs dynamically

--- app/components/StarRatingInput/index.tsx
+++ app/components/StarRatingInput/index.tsx
@@ -2,6 +2,7 @@ import type { ChangeEventHandler } from 'react';
 import { useState } from 'react';
 
 import { StarRating } from '~/components/StarRating';
+import { RATING_MAX } from '~/lib/constants';
 
 type Props = {
   defaultValue?: string;
@@ -25,96 +26,28 @@ export const StarRatingInput = ({ defaultValue }: Props) => {
       <div className="relative">
         <StarRating rating={rating} size={48} />
         <div className="absolute inset-0 flex font-mono">
-          <div>
-            <input
-              className="peer sr-only"
-              onChange={handleOnChange}
-              type="radio"
-              name="rating"
-              id="rating-1"
-              value="1"
-              defaultChecked={isChecked(defaultValue, '1')}
-              required
-            />
-            <label
-              htmlFor="rating-1"
-              className="flex h-12 w-12 cursor-pointer items-center justify-center pt-0.5 peer-focus-visible:ring"
-            >
-              1
-            </label>
-          </div>
-          <div>
-            <input
-              className="peer sr-only"
-              onChange={handleOnChange}
-              type="radio"
-              name="rating"
-              id="rating-2"
-              value="2"
-              defaultChecked={isChecked(defaultValue, '2')}
-              required
-            />
-            <label
-              htmlFor="rating-2"
-              className="flex h-12 w-12 cursor-pointer items-center justify-center pt-0.5 peer-focus-visible:ring"
-            >
-              2
-            </label>
-          </div>
-          <div>
-            <input
-              className="peer sr-only"
-              onChange={handleOnChange}
-              type="radio"
-              name="rating"
-              id="rating-3"
-              value="3"
-              defaultChecked={isChecked(defaultValue, '3')}
-              required
-            />
-            <label
-              htmlFor="rating-3"
-              className="flex h-12 w-12 cursor-pointer items-center justify-center pt-0.5 peer-focus-visible:ring"
-            >
-              3
-            </label>
-          </div>
-          <div>
-            <input
-              className="peer sr-only"
-              onChange={handleOnChange}
-              type="radio"
-              name="rating"
-              id="rating-4"
-              value="4"
-              defaultChecked={isChecked(defaultValue, '4')}
-              required
-            />
-            <label
-              htmlFor="rating-4"
-              className="flex h-12 w-12 cursor-pointer items-center justify-center pt-0.5 peer-focus-visible:ring"
-            >
-              4
-            </label>
-          </div>
-          <div>
-            <input
-              className="peer sr-only"
-              onChange={handleOnChange}
-              type="radio"
-              name="rating"
-              id="rating-5"
-              value="5"
-              defaultChecked={isChecked(defaultValue, '5')}
-              required
-            />
-            <label
-              htmlFor="rating-5"
-              className="flex h-12 w-12 cursor-pointer items-center justify-center pt-0.5 peer-focus-visible:ring"
-            >
-              5
-            </label>
-          </div>
+          {Array.from({ length: RATING_MAX }, (_, index) => index + 1).map(
+            (value) => (
+              <div key={value}>
+                <input
+                  className="peer sr-only"
+                  onChange={handleOnChange}
+                  type="radio"
+                  name="rating"
+                  id={`rating-${value}`}
+                  value={String(value)}
+                  defaultChecked={isChecked(defaultValue, String(value))}
+                  required
+                />
+                <label
+                  htmlFor={`rating-${value}`}
+                  className="flex h-12 w-12 cursor-pointer items-center justify-center pt-0.5 peer-focus-visible:ring"
+                >
+                  {value}
+                </label>
+              </div>
+            ),
+          )}
         </div>
       </div>
     </fieldset>
