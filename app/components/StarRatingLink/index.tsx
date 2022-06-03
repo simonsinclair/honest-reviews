4bf83a94d05202e6414f3b315341bab0be6252f1
Simon Sinclair <simon@simonsinclair.com>
2022-06-03 13:17:50 +0100

    Add components/StarRatingLink

---
+++ app/components/StarRatingLink/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from '@remix-run/react';
+
+import { StarRating } from '~/components/StarRating';
+import { RATING_MAX } from '~/lib/constants';
+
+type Props = {
+  href: string;
+  rating: number;
+  ratingCount: number;
+};
+
+export const StarRatingLink = ({ rating, ratingCount, href }: Props) => (
+  <Link to={href} className="group flex items-center gap-2 leading-none">
+    <StarRating
+      rating={rating}
+      size={20}
+      ariaLabel={`Rated ${rating} out of ${RATING_MAX}.`}
+    />
+    <span aria-hidden className="opacity-75 before:mr-2 before:content-['•']">
+      {rating}
+    </span>
+    <span className="opacity-75 group-hover:underline">
+      ({ratingCount} reviews)
+    </span>
+  </Link>
+);
